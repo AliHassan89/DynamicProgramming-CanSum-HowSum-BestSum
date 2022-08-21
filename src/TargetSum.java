7951e33726d8e7f0d87395888ca37ee4d6e8728b
Ali <thealihassan@gmail.com>
2022-08-21 20:11:55 +0200

    memoized code for canSum, howSum and bestSum

--- src/TargetSum.java
+++ src/TargetSum.java
@@ -3,16 +3,72 @@ import java.util.*;
 public class TargetSum
 {
     public static void main(String[] args) {
-        int[] numbers = {1,2,5,25};
-        System.out.println(bestSumShortestArray(100, numbers, new HashMap<>()));
+        int[] numbers = {5,4,3,7};
+        System.out.println(canSum(7, numbers, new HashMap<>())); // expected result: true
+        System.out.println(howSum(7, numbers, new HashMap<>())); // expected result: [3,4]
+        System.out.println(bestSum(7, numbers, new HashMap<>())); // expected result: [7]
     }
 
-    private static boolean canSum(int targetSum, int[] numbers, Map<Integer, List<Integer>> memo)
+    private static boolean canSum(int targetSum, int[] numbers, Map<Integer, Boolean> memo)
     {
+        if (memo.get(targetSum) != null)
+        {
+            return memo.get(targetSum);
+        }
+        if (targetSum == 0)
+        {
+            return true;
+        }
+        else if (targetSum < 0)
+        {
+            return false;
+        }
+        for (int number : numbers)
+        {
+            int remainder = targetSum - number;
+            if (canSum(remainder, numbers, memo))
+            {
+                memo.put(targetSum, true);
+                return true;
+            }
+        }
 
+        memo.put(targetSum, false);
+        return false;
     }
 
-    private static List<Integer> bestSumShortestArray(int targetSum, int[] numbers, Map<Integer, List<Integer>> memo)
+    private static List<Integer> howSum(int targetSum, int[] numbers, Map<Integer, List<Integer>> memo)
+    {
+        if (memo.get(targetSum) != null)
+        {
+            return memo.get(targetSum);
+        }
+        else if (targetSum == 0)
+        {
+            return new ArrayList<>();
+        }
+        else if (targetSum < 0)
+        {
+            return null;
+        }
+        for (int number : numbers)
+        {
+            int remainder = targetSum - number;
+            List<Integer> remainderList = howSum(remainder, numbers, memo);
+            if (remainderList != null)
+            {
+                List<Integer> resultantList = new ArrayList<>(remainderList);
+                resultantList.add(number);
+                memo.put(targetSum, resultantList);
+                return resultantList;
+            }
+        }
+
+        memo.put(targetSum, null);
+        return null;
+    }
+
+    private static List<Integer> bestSum(int targetSum, int[] numbers, Map<Integer, List<Integer>> memo)
     {
         if (memo.get(targetSum) != null)
         {
@@ -32,7 +88,7 @@ public class TargetSum
         for (int number : numbers)
         {
             int remainder = targetSum - number;
-            List<Integer> remainderResult = bestSumShortestArray(remainder, numbers, memo);
+            List<Integer> remainderResult = bestSum(remainder, numbers, memo);
             if (remainderResult != null)
             {
                 List<Integer> remainderCombination = new ArrayList<>(remainderResult);
